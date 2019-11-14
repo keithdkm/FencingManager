9a5615ffca5d1c874945f9ca7095ce940098571b
blacklaws <kdkmbusiness@gmail.com>
2019-11-14 00:20:13 -0500

    Fetch database connection string from .env

--- config.py
+++ config.py
@@ -2,15 +2,19 @@
 Flask-SQLalchemy config Class
 '''
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     '''
     Config class for FlaskSQLAlchemy
     '''
+    
     # defines location for database,   
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'fencing_competitions.db')
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'fencing_competitions.db')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
