8a7b1d95fca915066794142905a94268cf857c87
blacklaws <kdkmbusiness@gmail.com>
2019-11-23 21:50:09 -0500

    Debug info added to previous code

--- fencingapp/data_load/members.py
+++ fencingapp/data_load/members.py
@@ -22,7 +22,7 @@ def load_members_to_db_from_csv():
                     #  "Background Check Expires",
                     #  "SafeSport Expires"
                       ]
-    Member_table_empty = Member.query.count()==0
+    Member_table_empty = True
     # Fetch a fresh URL link for the current member list file
     THIS_SEASONS_MEMBERS = memberFile(USFA_URLs.USFA_MEMBER_LIST).url_link
     member_file_list=[THIS_SEASONS_MEMBERS]
@@ -31,11 +31,12 @@ def load_members_to_db_from_csv():
         basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'data')
         MEMBERS1819 = Path(basedir,'members1819.csv')  
         member_file_list.append(MEMBERS1819)
-
+    print(member_file_list)
     # Load data from csv files and concatenate it together
        # files containing the member data 
     member_data = pd.DataFrame()   # Empty dataframe 
     for member_file in member_file_list: #concatenate this year's and last year's membership list
+        print(f'fetching {member_file}')
         member_data = member_data.append(
                         pd.read_csv(member_file, # this season's members
                                 header = 0 ,
@@ -63,14 +64,17 @@ def load_members_to_db_from_csv():
     member_data['updated_on'] = member_data['created_on'] = dt.utcnow()
 
     #clear the existing contents of the member table
+    print('dropping user table constraint')
     db.engine.execute('ALTER TABLE users DROP CONSTRAINT users_member_id_fkey')  # drop FK constraint with user table
     db.session.commit()
+    print("deleting old table contents")
     Member.query.delete()
     db.session.commit()
     # add back FK constraint
     db.engine.execute('ALTER TABLE users ADD CONSTRAINT users_member_id_fkey FOREIGN KEY(member_id) REFERENCES members (id_)')
     db.session.commit()
-
+    print('reinstating user table constraint')
+    
     try:
         member_data.to_sql('members',db.engine,if_exists='append',index=False)
     except Exception as e:
